3cf989aef68fbf91d76724e3a053c22969e538ec
joaoandreossi <matusaboh@gmail.com>
2021-02-03 19:20:12 -0300

    comments: Add the time complexity of Stack operations

--- DataStructures/Stack/Stack.js
+++ DataStructures/Stack/Stack.js
@@ -10,22 +10,37 @@ Object.defineProperty(Stack.prototype, 'length', {
     }
 })
 
+/*
+*   Time complexity: O(1)
+*/
 Stack.prototype.push = function(val){
     return this.array.push(val)
 }
 
+/*
+*  Time complexity: O(1)
+*/
 Stack.prototype.pop = function(){
     return this.array.pop()
 }
 
+/*
+*   Time complexity: O(1)
+*/
 Stack.prototype.peek = function(){
     return this.array.length !== 0 ? this.array[this.array.length-1] : undefined
 }
 
+/*
+*   Time complexity: O(1)
+*/
 Stack.prototype.print = function(){
     console.log(this.array)
 }
 
+/*
+*   Time complexity: O(n)
+*/
 Stack.prototype.contains = function(val){
     let contain = false
     this.array.forEach((element) => {
