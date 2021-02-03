d0f588166369a7ac4ffb6e552c4c8cfa9b14f32e
joaoandreossi <matusaboh@gmail.com>
2021-02-03 12:17:34 -0300

    tests: Make more descriptive tests for the Stack

--- DataStructures/Stack/Stack.test.js
+++ DataStructures/Stack/Stack.test.js
@@ -1,16 +1,36 @@
 const Stack = require('./Stack')
 
-const stack = new Stack()
-
-test('Stack', () => {
-    expect(stack.push(1)).toBe(1)
-    expect(stack.push(1)).toBe(2)
-    expect(stack.length).toBe(2)
-    expect(stack.contains(1)).toBe(true)
-    expect(stack.contains(2)).toBe(false)
-    expect(stack.pop()).toBe(1)
+test('When an empty stack is popped it should return undefined', () => {
+    const stack = new Stack()
+    stack.push(1)
     expect(stack.pop()).toBe(1)
     expect(stack.pop()).toBe(undefined)
-    expect(stack.contains(1)).toBe(false)
-    expect(stack.length).toBe(0)
+})
+
+test('The stack should follow FIFO rules', () => {
+    const stack = new Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.pop()).toBe(3)
+    expect(stack.pop()).toBe(2)
+    expect(stack.pop()).toBe(1)
+    expect(stack.pop()).toBe(undefined)
+})
+
+test('contains() should correctly indicate the presence of the passed value', () => {
+    const stack = new Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.contains(3)).toBe(true)
+    expect(stack.contains(5)).toBe(false)
+})
+
+test('The length property should show the correct size of the stack', () => {
+    const stack = new Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.length).toBe(3)
 })
